0c4c6ff46114b4635d92e5559aaff0b9787d45f2
Stanislav Pugach <staspugachover900@gmail.com>
2016-07-19 19:32:48 +0300

    Level2 lesson02 task02 + magic

--- JavaRushHomeWork/src/com/javarush/test/level14/lesson08/home01/Bridge.java
+++
@@ -1,8 +0,0 @@
-package com.javarush.test.level14.lesson08.home01;
-
-/**
- * Created by Стас on 08.04.2015.
- */
-public interface Bridge {
-    int getCarsCount();
-}
--- JavaRushHomeWork/src/com/javarush/test/level14/lesson08/home05/CompItem.java
+++
@@ -1,9 +0,0 @@
-package com.javarush.test.level14.lesson08.home05;
-
-/**
- * Created by Стас on 14.04.2015.
- */
-public interface CompItem
-{
-    public String getName();
-}
