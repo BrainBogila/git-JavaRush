9325385c11011b6cad78d4f953cbb4fb938644b9
Stanislav Pugach <staspugachover900@gmail.com>
2016-07-22 17:06:36 +0300

    level20.lesson02.task03

--- JavaRushHomeWork/src/com/javarush/test/level20/lesson02/task02/Solution.java
+++ JavaRushHomeWork/src/com/javarush/test/level20/lesson02/task02/Solution.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 /* Читаем и пишем в файл: JavaRush
 Реализуйте логику записи в файл и чтения из файла для класса JavaRush
@@ -24,13 +25,16 @@ public class Solution
 
             JavaRush javaRush = new JavaRush();
             //initialize users field for the javaRush object here - инициализируйте поле users для объекта javaRush тут
-            User user = new User();
-            user.setFirstName("Stas");
-            user.setLastName("Pugach");
-            user.setBirthDate(new Date(123456789));
-            user.setMale(true);
-            user.setCountry(User.Country.UKRAINE);
-            javaRush.users.add(user);
+            for (int i = 0; i < 5; i++)
+            {
+                User user = new User();
+                user.setFirstName("Stas" + i);
+                user.setLastName("Pugach" + i);
+                user.setBirthDate(new Date(123456789));
+                user.setMale(true);
+                user.setCountry(User.Country.UKRAINE);
+                javaRush.users.add(user);
+            }
 
             javaRush.save(outputStream);
             outputStream.flush();
@@ -38,16 +42,15 @@ public class Solution
             JavaRush loadedObject = new JavaRush();
             loadedObject.load(inputStream);
             //check here that javaRush object equals to loadedObject object - проверьте тут, что javaRush и loadedObject равны
-            User user1 = loadedObject.users.get(0);
-            System.out.println(user1.getFirstName()+
-                    " "+user1.getLastName()+
-                    " "+user1.getBirthDate()+
-                    " "+user1.getCountry()+
-                    " "+user1.isMale()
-            );
-            
-
-
+            for (User num : loadedObject.users)
+            {
+                System.out.println(num.getFirstName() +
+                        " " + num.getLastName() +
+                        " " + num.getBirthDate() +
+                        " " + num.getCountry() +
+                        " " + num.isMale()
+                );
+            }
             outputStream.close();
             inputStream.close();
 
@@ -73,68 +76,70 @@ public class Solution
             //implement this method - реализуйте этот метод
             PrintWriter writer = new PrintWriter(outputStream);
 
-            if (!users.isEmpty()){
-                for (User currentUser : users)
+            for (int i = 0; i < users.size(); i++)
+            {
+                if (users.get(i).getFirstName() != null)
                 {
-                    if (currentUser.getFirstName() != null){
-                        writer.println("yes");
-                        writer.println(currentUser.getFirstName());
-                    }
-                    else writer.println("no");
+                    writer.println("yes");
+                    writer.println(users.get(i).getFirstName());
+                } else writer.println("no");
 
-                    if (currentUser.getLastName() != null){
-                        writer.println("yes");
-                        writer.println(currentUser.getLastName());
-                    }
-                    else writer.println("no");
+                if (users.get(i).getLastName() != null)
+                {
+                    writer.println("yes");
+                    writer.println(users.get(i).getLastName());
+                } else writer.println("no");
 
-                    if (currentUser.getBirthDate() != null){
-                        writer.println("yes");
-                        Date date = currentUser.getBirthDate();
-                        writer.println(date.getTime());
-                    }
-                    else writer.println("no");
+                if (users.get(i).getBirthDate() != null)
+                {
+                    writer.println("yes");
+                    Date date = users.get(i).getBirthDate();
+                    writer.println(date.getTime());
+                } else writer.println("no");
 
-                    writer.println(currentUser.isMale());
+                writer.println(users.get(i).isMale());
 
-                    if (currentUser.getCountry() != null){
-                        writer.println("yes");
-                        writer.println(currentUser.getCountry());
-                    }
-                    else writer.println("no");
-                    writer.flush();
-                    writer.close();
-                }
+                if (users.get(i).getCountry() != null)
+                {
+                    writer.println("yes");
+                    writer.println(users.get(i).getCountry());
+                } else writer.println("no");
             }
-
+            writer.flush();
+            writer.close();
         }
 
         public void load(InputStream inputStream) throws Exception
         {
             //implement this method - реализуйте этот метод
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            User user = new User();
-            String isFirstNamePresent = reader.readLine();
-            if (isFirstNamePresent.equals("yes"))
-                user.setFirstName(reader.readLine());
-            String isLastNamePresent = reader.readLine();
-            if (isLastNamePresent.equals("yes"))
-                user.setLastName(reader.readLine());
-            String isBirthDatePresent = reader.readLine();
-            if (isBirthDatePresent.equals("yes")){
-                Date date = new Date(Long.parseLong(reader.readLine()));
-                user.setBirthDate(date);
+            Scanner scanner  = new Scanner(inputStream);
+            while (scanner.hasNext())
+            {
+                User user = new User();
+                String isFirstNamePresent = scanner .nextLine();
+                if (isFirstNamePresent.equals("yes"))
+                    user.setFirstName(scanner .nextLine());
+                String isLastNamePresent = scanner .nextLine();
+                if (isLastNamePresent.equals("yes"))
+                    user.setLastName(scanner .nextLine());
+                String isBirthDatePresent = scanner .nextLine();
+                if (isBirthDatePresent.equals("yes"))
+                {
+                    Date date = new Date(Long.parseLong(scanner .nextLine()));
+                    user.setBirthDate(date);
+                }
+                String Male = scanner .nextLine();
+                if (Male.equals("true"))
+                    user.setMale(true);
+                else user.setMale(false);
+                String isCountryPresent = scanner .nextLine();
+                if (isCountryPresent.equals("yes"))
+                    user.setCountry(User.Country.valueOf(scanner .nextLine()));
+                users.add(user);
             }
-            String Male = reader.readLine();
-            if (Male.equals("true"))
-                user.setMale(true);
-            else user.setMale(false);
-            String isCountryPresent = reader.readLine();
-            if (isCountryPresent.equals("yes"))
-                user.setCountry(User.Country.valueOf(reader.readLine()));
-            users.add(user);
-            reader.close();
         }
+
     }
+
 }
+
